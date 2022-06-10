1e39d2fea6e64ff1b1d72d9371767fd0bce3d299
Kyle S <sodacovaa@gmail.com>
2022-06-09 21:13:31 -0400

    Add beg command

---
+++ src/modules/Social/commands/Beg.js
@@ -0,0 +1,70 @@
+const { Command, CommandOptions } = require('axoncore');
+const profile = require('../../../Models/Profile');
+
+class Beg extends Command {
+    /**
+     * @param {import('axoncore').Module} module
+     */
+    constructor(module) {
+        super(module);
+
+        this.label = 'beg';
+        this.aliases = [
+        ];
+
+        this.hasSubcmd = false;
+
+        this.info = {
+            name: 'beg',
+            description: 'Want to earn money some more coins? Why don\'t you try begging, maybe someone will give you some.',
+            usage: 'beg',
+        };
+
+        /**
+         * @type {CommandOptions}
+         */
+        this.options = new CommandOptions(this, {
+            argsMin: 0,
+            cooldown: 60000,
+            guildOnly: true,
+        } );
+    }
+    /**
+     * @param {import('axoncore').CommandEnvironment} env
+     */
+
+    async execute({ msg }) {
+
+        profile.findById(msg.author.id, (err, doc) => {
+            if (err) {
+                return this.sendError(msg.channel, `DB Error: ${err}`);
+            } else if (!doc || doc.data.economy.wallet === null) {
+                return this.sendError(msg.channel, `You don't have a wallet yet! To create one, run \`${this.axon.settings.prefixes}register.`);
+            } else {
+                const now = Date.now();
+                const duration = 3.6e+6;
+                const userProfile = doc.data.economy.beg;
+                let overflow = false, excess = null;
+
+                if (userProfile.date > now) {
+                    return this.sendError(msg.channel, `You have already been given some coins earlier! Please try again later.`);
+                };
+
+                userProfile.date = Date.now() + duration;
+                const amount = Math.floor(Math.random() * 200) + 100;
+
+                if (doc.data.economy.wallet + amount > 50000) {
+                    overflow = true;
+                    excess = doc.data.economy.wallet + amount - 50000
+                };
+
+                doc.data.economy.wallet = overflow ? 50000 : doc.data.economy.wallet + amount;
+
+                return doc.save().then(() => msg.channel.createMessage([`${this.utils.emote.success} You received **${amount}** from me.`, overflow ? `**Overflow warning**! Please deposit some of your account to your bank. You only received ${amount - excess} for this one!` : '',].join('')))
+                .catch((err) => this.sendError(msg.channel, err));
+            }
+        })
+    }
+}
+
+module.exports = Beg;
--- src/modules/Social/commands/index.js
+++ src/modules/Social/commands/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     Balance: require('./Balance'),
     Bank: require('./Bank'),
+    Beg: require('./Beg'),
     Daily: require('./Daily'),
     Register: require('./Register')
 }
