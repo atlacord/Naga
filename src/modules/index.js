2d4abb9398f6312659e8a516fe76ef2694fba0ed
soda <37189894+sodacova@users.noreply.github.com>
2022-06-10 03:06:50 -0400

    Add social module to index.js
    Forgot to include this in my pr

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,5 +4,6 @@ module.exports = {
     Info: require('./Info/index'),
     Manager: require('./Manager/index'),
     Misc: require('./Misc/index'),
+    Social: require('./Social/index'),
     Utilities: require('./Utilities/index')
 };
