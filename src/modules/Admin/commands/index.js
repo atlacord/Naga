43daadd36b657a8d485ca89c36837de6165c4a7d
Kyle S <sodacovaa@gmail.com>
2022-06-20 00:09:21 -0400

    Add GetMessage command for troubleshooting purposes

--- src/modules/Admin/commands/index.js
+++ src/modules/Admin/commands/index.js
@@ -2,6 +2,7 @@ module.exports = {
     Build: require('./Build'),
     Eval: require('./Eval'),
     Exec: require('./Exec'),
+    GetMessage: require('./GetMessage'),
     Restart: require('./Restart'),
     Setgame: require('./Setgame'),
 };
