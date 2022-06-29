791115fad6a89ca4b08c97ce1ac2b2f292901b28
Nick J <nmj3@hotmail.com>
2022-06-29 12:03:17 +1200

    Made constants more concise; fixed channel finding

--- src/modules/Info/listeners/TeamAvatar.js
+++ src/modules/Info/listeners/TeamAvatar.js
@@ -26,15 +26,16 @@ class TeamAvatar extends Listener {
      * @param {import('axoncore').GuildConfig} guildConfig
      */
     execute(member, oldMember, guildConfig) { // eslint-disable-line
+        const TAChannel = server.channels.find(c => c.id === '826851222459514923')
+        const server = guildConfig.guildID
+
         if(!oldMember.roles.find(r => r.name === 'Team Avatar') && member.roles.find(r => r.name === 'Team Avatar')) {
-            const server = guildConfig.guildID
-          return  server.channel.find(c => c.id === '826851222459514923').then(channel.createMessage(`**A new member joins the fold! A big thanks to ${member.mention} for boosting the server! Please make sure you read the pins for info on how to get the TA colour role and more!`))
+        TAChannel.createMessage(`**A new member joins the fold! A big thanks to ${member.mention} for boosting the server! Please make sure you read the pins for info on how to get the TA colour role and more!`)
         }
         if(oldMember.roles.find(r => r.name === 'Team Avatar') && oldMember.roles.find(r => r.name === 'TA Colour') && !member.roles.find(r => r.name === 'Team Avatar')) {
-            const server = guildConfig.guildID
             const colourrole = server.roles.find(r => r.name === 'TA Colour')
             server.guild.removeMemberRole(member.id, colourrole.id, "User no longer boosting")
-            return  server.channel.find(c => c.id === '826851222459514923').then(channel.createMessage(`**A member of TA has left us, thanks to ${member.mention} for their contributions!`))
+            TAChannel.createMessage(`**A member of TA has left us, thanks to ${member.mention} for their contributions!`)
         }
 
         return Promise.resolve();
