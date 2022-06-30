aad18207483c10a3be3a1353634a14cf10fed26b
Kyle S <sodacovaa@gmail.com>
2022-06-29 22:28:54 -0400

    Add contest winner role tracking

--- src/modules/Info/listeners/index.js
+++ src/modules/Info/listeners/index.js
@@ -1,6 +1,7 @@
 module.exports = {
+    ContestWinner: require('./ContestWinner.js'),
 //    MessageCreateLog: require('./MessageCreateLog'),
 //    MessageCreateMod: require('./MessageCreateMod'),
-    Ready: require('./Ready'),
+//    Ready: require('./Ready'),
     TeamAvatar: require('./TeamAvatar.js')
 };
