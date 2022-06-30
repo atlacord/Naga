1f41fbb07be6108032ebf32dcb73b93b0458b91f
soda <37189894+sodacova@users.noreply.github.com>
2022-06-30 04:18:57 -0400

    Update index.js

--- src/modules/Info/listeners/index.js
+++ src/modules/Info/listeners/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-//    ContestWinner: require('./ContestWinner.js'),
-    ExistingTeamAvatar: require('./ExistingTeamAvatar.js')
+      ContestWinner: require('./ContestWinner.js'),
+      ExistingTeamAvatar: require('./ExistingTeamAvatar.js'),
 //    MessageCreateLog: require('./MessageCreateLog'),
 //    MessageCreateMod: require('./MessageCreateMod'),
 //    Ready: require('./Ready'),
-//    RuleScreening: require('./RuleScreening.js'),
-//    TeamAvatar: require('./TeamAvatar.js')
+      RuleScreening: require('./RuleScreening.js'),
+      TeamAvatar: require('./TeamAvatar.js')
 };
