5a488a372e65cbbb2f47f12281cd851c37a3ebd4
Kyle S <sodacovaa@gmail.com>
2022-09-21 20:20:31 -0400

    Search for messages from carl rather than an array of message ids

--- src/modules/Admin/commands/ScrapeLeaderboard.js
+++ src/modules/Admin/commands/ScrapeLeaderboard.js
@@ -1,7 +1,7 @@
 const { Command, CommandOptions, CommandPermissions } = require('axoncore');
 const { Tatsu } = require('tatsu');
 
-const client = new Tatsu('Q6EWLNtiZI-lBbxLXQLoiD1myInRiqJT5');
+const tatsu = new Tatsu('Q6EWLNtiZI-lBbxLXQLoiD1myInRiqJT5');
 
 // const userRegex = /<@([^}]+)>/g;
 
@@ -47,29 +47,50 @@ class ScrapeLeaderboard extends Command {
 
     async execute({msg, args }) {
 
-        let messages = [ '1013630746809880678', '1013630758465831075', '1013630770352488488', '1013630781597438053', '1013630792481656862', 
-        '1013630802506043402', '1013630815869087844', '1013630824702292008', '1013630859657625601', '1013630872890650674' ]; // I have no idea how we're going to do the whole thing lmao
+        // let messages = [ '1013630746809880678', '1013630758465831075', '1013630770352488488', '1013630781597438053', '1013630792481656862', 
+        // '1013630802506043402', '1013630815869087844', '1013630824702292008', '1013630859657625601', '1013630872890650674' ]; // I have no idea how we're going to do the whole thing lmao
 
         try {
 
-            for (let a in messages) {
-                let message = await this.bot.getMessage('1013630721774059550', messages[a])
-                message = message.embeds[0].description;
-                message = message.split('\n')
-                let newmessage;
-                msg.channel.createMessage(`Page ${parseInt(a) + 1}`)
-                for (let i in message) {
-                    newmessage = message[i].split(' ');
-                    newmessage.pop();
-                    newmessage.pop();
-                    newmessage.shift();
-                    // newmessage = message[i].slice(4);
-                    newmessage[0] = newmessage[0].replace(/<@|>/g, '') // .slice(0, -9).replace(',', '').split(' ')
-                    newmessage[1] = newmessage[1].replace(',', '').replace('xp', '');
+            let messages = await this.bot.getChannel('1013630721774059550').getMessages();
+            this.sendSuccess(msg.channel, `Exporting the Carl leaderboard to Tatsu.\nEstimated time: **${~~(20 * messages.length * (2000 / 1000))} seconds**`)
 
-                    // client.addGuildMemberScore(msg.guildID, newmessage[0]. parseInt(newmessage[1])); // Adds score to Tatsu
-                    
-                    msg.channel.createMessage(`\`${newmessage.toString()}\``);
+            messages = messages.reverse(); // Reverse the message array, transfer the top of the leaderboard first
+
+            for (let a in messages) {
+                if (messages[a].author.id === '235148962103951360') {
+
+                    let message = messages[a];
+                    message = message.embeds[0].description;
+                    message = message.split('\n')
+                    let newmessage;
+                    msg.channel.createMessage(`__**Leaderboard Page ${parseInt(messages[a].embeds[0].footer.text.slice(4, -5))}**__`);
+                    for (let i in message) {
+                        newmessage = message[i].split(' ');
+                        newmessage.pop();
+                        newmessage.pop();
+                        newmessage.shift();
+                        // newmessage = message[i].slice(4);
+                        newmessage[0] = newmessage[0].replace(/<@|>/g, '') // .slice(0, -9).replace(',', '').split(' ')
+                        newmessage[1] = newmessage[1].replace(',', '').replace('xp', '');
+                        newmessage[1] = newmessage[1].replace(',', '');
+
+                        let member = await this.bot.getRESTUser(newmessage[0])
+
+                        let embed = {
+                            color: this.utils.getColor('green'),
+                            description: `Transferring **${parseInt(newmessage[1]).toLocaleString()}** XP for **${member.username}#${member.discriminator}**.`,
+                            footer: { text: `ID: ${member.id}` },
+                            timestamp: new Date()
+                        }
+                        
+                        msg.channel.createMessage({embed})
+
+                        tatsu.addGuildMemberScore(msg.guildID, newmessage[0]. parseInt(newmessage[1])); // Adds score to Tatsu
+                            
+                        // msg.channel.createMessage(`\`${newmessage.toString()}\``);
+                        await this.utils.delayFor(2000); // Delays function to avoid getting ratelimited by Tatsu
+                    }
                 }
             }
         } catch (err) {
