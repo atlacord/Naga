d02513f56b92b71f8f076c82694a795ce4241fb7
Kyle S <sodacovaa@gmail.com>
2022-10-02 17:17:59 -0400

    Disable haiku command

--- src/modules/Fun/commands/index.js
+++ src/modules/Fun/commands/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     Dog: require('./Dog'),
-    Haiku: require('./Haiku'),
+    // Haiku: require('./Haiku'),
     Iroh: require('./Iroh'),
     Topic: require('./Topic'),
     Wyr: require('./Wyr')
