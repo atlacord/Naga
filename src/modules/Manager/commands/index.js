3f79d887067681baf3809dec472642c365468dba
Kyle S <kyle@soda.gg>
2022-12-11 21:12:06 -0500

    Add getmessage command

--- src/modules/Manager/commands/index.js
+++ src/modules/Manager/commands/index.js
@@ -3,6 +3,7 @@ module.exports = {
     ArchiveAll: require('./ArchiveAll'),
     ArchiveImages: require('./ArchiveImages'),
     GetArchive: require('./GetArchive'),
+    GetMessage:require('./GetMessage'),
     LoadSuggestion: require('./LoadSuggestion'),
     Members: require('./Members'),
     Nick: require('./Nick'),
