991c8484c62ef8b22ec2293af321ceabc889ce06
Kyle S <kyle@soda.gg>
2023-01-21 18:07:08 -0500

    Disable EnforceBending event

--- src/modules/Events/listeners/index.js
+++ src/modules/Events/listeners/index.js
@@ -1,7 +1,7 @@
 module.exports = {
      CheckServer: require('../Misc/CheckServer'),
      ContestWinner: require('../RoleHandling/ContestWinner.js'),
-     EnforceBending: require('../RoleHandling/EnforceBending.js'),
+     // EnforceBending: require('../RoleHandling/EnforceBending.js'),
      ExistingTeamAvatar: require('./ExistingTeamAvatar.js'),
      // Ready: require('./Ready'),
      RuleScreening: require('../RoleHandling/RuleScreening.js'),
