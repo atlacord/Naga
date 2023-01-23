5a849f5af2c5bbfa6df42401a35f056b6d06d2e8
Kyle <37189894+eaobao@users.noreply.github.com>
2023-01-22 21:08:25 -0500

    Re-enable events

--- src/modules/Events/Events.js
+++ src/modules/Events/Events.js
@@ -1,9 +1,9 @@
 module.exports = {
     CheckServer: require('./Misc/CheckServer'),
-    // ContestWinner: require('./RoleHandling/ContestWinner.js'),
+    ContestWinner: require('./RoleHandling/ContestWinner.js'),
     EnforceBending: require('./RoleHandling/EnforceBending.js'),
-    // ExistingTeamAvatar: require('./ExistingTeamAvatar.js'),
+    ExistingTeamAvatar: require('./ExistingTeamAvatar.js'),
     // Ready: require('./Ready'),
-    // RuleScreening: require('./RoleHandling/RuleScreening.js'),
-    // TeamAvatar: require('./TeamAvatar.js')
+    RuleScreening: require('./RoleHandling/RuleScreening.js'),
+    TeamAvatar: require('./TeamAvatar.js')
 };
