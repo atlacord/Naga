2f426e8af3d68f843f353ad4c5d9c50904a728f3
Kyle S <kyle@soda.gg>
2023-01-22 21:15:00 -0500

    Fix file issue

--- src/modules/Events/Events.js
+++ src/modules/Events/Events.js
@@ -1,9 +1,9 @@
 module.exports = {
     CheckServer: require('./Misc/CheckServer'),
-    // ContestWinner: require('./RoleHandling/ContestWinner.js'),
+    ContestWinner: require('./RoleHandling/ContestWinner.js'),
     EnforceBending: require('./RoleHandling/EnforceBending.js'),
-    // ExistingTeamAvatar: require('./Misc/ExistingTeamAvatar.js'),
+    ExistingTeamAvatar: require('./Misc/ExistingTeamAvatar.js'),
     // Ready: require('.Misc/Ready'),
-    // RuleScreening: require('./RoleHandling/RuleScreening.js'),
-    // TeamAvatar: require('./Misc/TeamAvatar.js')
+    RuleScreening: require('./RoleHandling/RuleScreening.js'),
+    TeamAvatar: require('./Misc/TeamAvatar.js')
 };
