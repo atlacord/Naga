148195c9a0ccfca3c281dd07983f76c5774ec383
Kyle S <kyle@soda.gg>
2023-05-11 18:09:42 -0400

    Add blurple canvas event

---
+++ src/modules/Events/Misc/BlurpleCanvas.js
@@ -0,0 +1,37 @@
+const { Listener } = require('axoncore');
+
+const BLURPLE_ROLE = '1106341185229951016'
+
+class BlurpleCanvas extends Listener {
+    /**
+     * @param {import('axoncore').Module} module
+     * @param {import('axoncore').ListenerData} data
+     */
+    constructor(module, data = {} ) {
+        super(module, data);
+
+        /** Event Name (Discord name) */
+        this.eventName = 'messageCreate';
+        /** Event name (Function name) */
+        this.label = 'blurpleCanvas';
+
+        this.enabled = true;
+
+        this.info = {
+            description: 'Grants a role to users who participate in Blurple Canvas',
+        };
+    }
+
+    /**
+     * @param {import('eris').Message} msg
+     */
+
+    async execute(msg) { // eslint-disable-line
+        if (msg.author.bot) return;
+        if ((msg.content.startsWith('p/place')) && ((msg.channel.id === '1105276831340433438') || (msg.channel.id === '372087473892884502'))) {//'—ban' || '--ban')) {
+            msg.channel.guild.addMemberRole(msg.author.id, BLURPLE_ROLE, 'Participated in Blurple Canvas 2022')
+        }
+    }
+}
+
+module.exports = BlurpleCanvas;
