a32dbb56762d25851312395e22fdccae0b462215
Cap <61604923+CaptainM777@users.noreply.github.com>
2023-06-15 20:15:09 -0400

    Replaced old rules channel with new one

--- src/modules/Events/Interactions/WelcomeMessage.js
+++ src/modules/Events/Interactions/WelcomeMessage.js
@@ -77,7 +77,7 @@ class WelcomeMessage extends Listener {
                                
                         \n\n<@&586128911302131725> – An honorary role given automatically to server boosters. Grants access to <#826851222459514923>, our private chat channel for Team Avatar members.
                                
-                        \n\n<@&433112551195279360> – A role used to temporarily mute server members who misbehave or break our rules (see <#728364994005303307>), preventing them from typing in our chats. This role is removed automatically once the mute duration has been served. To find your mute reason and duration, check the DM you should have received from <@!424659040170409984> (you need to have your DMs open to receive this).
+                        \n\n<@&433112551195279360> – A role used to temporarily mute server members who misbehave or break our rules (see <#1066580298290176121>), preventing them from typing in our chats. This role is removed automatically once the mute duration has been served. To find your mute reason and duration, check the DM you should have received from <@!424659040170409984> (you need to have your DMs open to receive this).
                                
                         \n\n<@&717748188630482946> – A role used to revoke access to <#812409753602883626> and <#388122648854528001> for server members who misuse those channels. This role also prevents users from re-assigning themselves the <@&991902499516731494> and <@&388121551779921930> roles (see “Channel Access Roles” on our community onboarding page).`
                     }
