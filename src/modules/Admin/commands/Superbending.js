ddc1308919ea6ffbaea27b3c2058a6cb7c56d904
Kyle <37189894+kylespadaro@users.noreply.github.com>
2023-12-03 18:56:06 -0500

    Update Superbending.js

--- src/modules/Admin/commands/Superbending.js
+++ src/modules/Admin/commands/Superbending.js
@@ -47,30 +47,28 @@ class Superbending extends Command {
             }
 
             for (let i in members) {
-                console.log(members[i].username, members[i].roles);
-
                 if (members[i].roles.includes('372085492910522370')) {
-                    await this.bot.addGuildMemberRole('370708369951948800', member.id, '1180969376770441298', 'Added super-waterbending role');
+                    await this.bot.addGuildMemberRole('370708369951948800', members[i].id, '1180969376770441298', 'Added super-waterbending role');
                     console.log('water');
                 };
 
                 if (members[i].roles.includes('372085752319967236')) {
-                    await this.bot.addGuildMemberRole('370708369951948800', member.id, '1180969390049607791', 'Added super-earthbending role');
+                    await this.bot.addGuildMemberRole('370708369951948800', members[i].id, '1180969390049607791', 'Added super-earthbending role');
                     console.log('earth');
                 };
 
                 if (members[i].roles.includes('1172722062876495963')) {
-                    await this.bot.addGuildMemberRole('370708369951948800', member.id, '1180969386245378058', 'Added super-firebending role');
+                    await this.bot.addGuildMemberRole('370708369951948800', members[i].id, '1180969386245378058', 'Added super-firebending role');
                     console.log('fire');
                 };
 
                 if (members[i].roles.includes('372085326165835777')) {
-                    await this.bot.addGuildMemberRole('370708369951948800', member.id, '1180969398048129166', 'Added super-airbending role');
+                    await this.bot.addGuildMemberRole('370708369951948800', members[i].id, '1180969398048129166', 'Added super-airbending role');
                     console.log('air');
                 };
 
                 if (members[i].roles.includes('372093851600683011')) {
-                    await this.bot.addGuildMemberRole('370708369951948800', member.id, '1180969393841242194', 'Added super-nonbending role');
+                    await this.bot.addGuildMemberRole('370708369951948800', members[i].id, '1180969393841242194', 'Added super-nonbending role');
                     console.log('non');
                 };
             };
