4e2914ab2a50f15ca54e7442f8ec128c6792ec6a
Cap <61604923+CaptainM777@users.noreply.github.com>
2024-08-21 20:49:16 -0400

    Modify haiku command for contest and enable

--- src/modules/Fun/commands/Haiku.js
+++ src/modules/Fun/commands/Haiku.js
@@ -16,7 +16,7 @@ class Haiku extends Command {
 
         this.info = {
             name: 'haiku',
-            description: 'Can you hack it in the Five-Seven-Five society?',
+            description: 'Can you hack it in the Five-Seven-Six society?',
             usage: 'haiku [your haiku here]',
         };
 
@@ -29,13 +29,9 @@ class Haiku extends Command {
             guildOnly: true,
         });
 
-        // this.permissions = new CommandPermissions(this, {
-        //     staff: {
-        //         needed: this.axon.staff.dailis,
-        //         bypass: this.axon.staff.owners,
-        //     },
-        //     custom: (msg) => (!msg.member.roles.includes('724751859356794880'))
-        // });
+        this.permissions = new CommandPermissions(this, {
+            custom: (msg) => (msg.channel.id === '1273986973450764363')
+        });
     }
     /**
      * @param {import('axoncore').CommandEnvironment} env
@@ -51,7 +47,7 @@ class Haiku extends Command {
         const syllables = this.lookupSyllables(lines);
       
         // 5 7 5
-        return syllables[0] === 5 && syllables[1] === 7 && syllables[2] === 5;
+        return syllables[0] === 5 && syllables[1] === 7 && syllables[2] === 6;
     }
 
     lookupSyllables (lines) {
@@ -69,19 +65,46 @@ class Haiku extends Command {
     }
 
     async execute({ msg, args }) {
-        let data = readFileSync('src/assets/haikus.json');
-        let haikus = JSON.parse(data);
-        if ((haikus.includes(args.join(' '))) || args.join(' ').includes('haiku')) {
-            return msg.channel.createMessage('Give me something more original!');
+        let data = JSON.parse(readFileSync('assets/haikus.json'));
+
+        let ids = data.map((obj) => obj.id);
+        let haikus = data.map((obj) => obj.haiku);
+
+        if (ids.includes(msg.member.id)) {
+            return msg.channel.createMessage({
+                embed: {
+                    color: this.utils.getColor('red'),
+                    title: '<:sokkajuiced:397018948253777931> You\'ve already submitted a haiku!'
+                }
+            });
         }
+        else if ((haikus.includes(args.join(' '))) || args.join(' ').includes('haiku')) {
+            return msg.channel.createMessage({
+                embed: {
+                    color: this.utils.getColor('red'),
+                    title: '<:sokkajuiced:397018948253777931> That haiku has already been submitted!'
+                }
+            });
+        }
+
         let res = this.haiku(args.join(' '));
-        if (res === true) {
-            msg.channel.createMessage('What a Remarkable Oaf!');
-            msg.member.addRole('724751859356794880', 'Sent a valid haiku');
-            haikus.push(args.join(' '));
-            writeFileSync('src/assets/haikus.json', JSON.stringify(haikus));
+        if (res) {
+            msg.channel.createMessage({ 
+                embed: {
+                    color: this.utils.getColor('green'),
+                    title: '<:sokkathumbsup:893152378864402473> Haiku accepted! :)'
+                }
+            });
+            // msg.member.addRole('724751859356794880', 'Sent a valid haiku');
+            data.push({ id: msg.member.id, haiku: args.join(' ') });
+            writeFileSync('assets/haikus.json', JSON.stringify(data, null, 3));
         } else {
-            msg.channel.createMessage('That was not a haiku :(');
+            msg.channel.createMessage({
+                embed: {
+                    color: this.utils.getColor('red'),
+                    title: '<:sokkasad:725833135757197417> That was not a Sokka haiku! :('
+                }
+            });
         }
     }
 }
