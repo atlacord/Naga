5ee38fcac802210ed78a754f40de9c4431c8e71f
Cap <61604923+CaptainM777@users.noreply.github.com>
2024-09-14 00:37:54 -0400

    Remove chi blocked role

--- src/modules/Events/Interactions/WelcomeMessage.js
+++ src/modules/Events/Interactions/WelcomeMessage.js
@@ -69,9 +69,7 @@ class WelcomeMessage extends Listener {
                                
                         \n\n<@&516790566017564684> – A temporary (1-month) role given to the winner(s) of contest events that we hold in the server. To get notifications for when such events are held, grab yourself the <@&497208797039689749> role (see “Notification Roles” below). After 1 month, this role is removed and replaced with <@&530972685610778635> instead.
                                
-                        \n\n<@&586128911302131725> – An honorary role given automatically to server boosters. Grants access to <#826851222459514923>, our private chat channel for Team Avatar members.
-                               
-                        \n\n<@&433112551195279360> – A role used to temporarily mute server members who misbehave or break our rules (see <#1066580298290176121>), preventing them from typing in our chats. This role is removed automatically once the mute duration has been served. To find your mute reason and duration, check the DM you should have received from <@!380453326216626176> (you need to have your DMs open to receive this).`
+                        \n\n<@&586128911302131725> – An honorary role given automatically to server boosters. Grants access to <#826851222459514923>, our private chat channel for Team Avatar members.`
                     }
                 ]
             })        
