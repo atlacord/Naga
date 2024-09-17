5ddf39164ba82e8c9041af10694649be6f8d7588
Kyle S <sodacovaa@gmail.com>
2024-09-16 22:44:39 -0400

    Remove 'Contact Staff' button

--- src/modules/Manager/commands/WelcomeButtons.js
+++ src/modules/Manager/commands/WelcomeButtons.js
@@ -42,22 +42,24 @@ class WelcomeButtons extends Command {
     async execute( { msg } ) {
         await this.bot.getChannel('1053064927935467530').editMessage('1066847898748338226', {
             embeds: [
-                {
+                /* {
                     color: 15913095,
                     image: {
                         url: `https://cdn.discordapp.com/attachments/411903716996677639/1051271170382635139/welcome.png`
                     } 
-                },
+                }, */
                 {
                     color: 15913095,
-                    description: `**Welcome to the Avatar: The Last Airbender Discord server!**
+                    description: `# Welcome to the Avatar: The Last Airbender Discord community!
                     \n\nThis is a Discord community for fans of the Avatar Legends universe (ATLA, Legend of Korra, and all other related media). This is a volunteer-run community to make a space for all fans of the show to come together. We are not associated with Nickelodeon or Avatar Studios.
                     \n\nFor any questions, feedback, concerns or reports, you can contact the server staff by messaging <@718577208687460482>, our modmail bot. A staff member will reply to you as soon as possible after you have sent a message. To handle the situation quickly and efficiently, please provide the staff with all relevant information.
 
                     \nWant to connect with us more? Check our socials below!
-                    \n[Wiki](https://wiki.atla.sh) - Run by our community, the wiki contains server lore and pages on our active users, events and groups!
+                    \n[Community Wiki](https://wiki.atla.sh) - Run by our community, the wiki contains server lore and pages on our active users, events and groups!
+                    \n[Instagram](https://instagram.com/atlacord)
+                    \n[TikTok](https://tiktok.com/@atlacord)
                     \n[Twitter](https://twitter.com/atlacord) 
-                    \n[Instagram](https://instagram.com/atlacord)`,
+                    \n[YouTube](https://youtube.com/@atlacord)`,
                 },
             ],
             components: [
@@ -88,12 +90,12 @@ class WelcomeButtons extends Command {
                             style: 1, 
                             custom_id: "TeamButton"
                         }, 
-                        {
+/*                         {
                             type: 2,
                             label: "Contact Staff", 
                             style: 5, 
                             url: `https://discord.com/users/718577208687460482`,
-                        }, 
+                        },  */
                     ]
                 }
             ]
