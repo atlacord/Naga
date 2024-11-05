56b5ef485bcabd0454d1b037fee54e2f0e15376f
Cap <61604923+CaptainM777@users.noreply.github.com>
2024-11-05 16:06:58 -0500

    Commit right file this time for previous commit (whoops)

---
+++ src/modules/Manager/commands/TopicsList.js
@@ -0,0 +1,62 @@
+const { Command, CommandPermissions } = require('axoncore');
+const server = require('../../../Models/Server.js');
+const embedPaginator = require('eris-pagination');
+
+class TopicsList extends Command {
+    /**
+     * @param {import('axoncore').Module} module
+     */
+    constructor(module) {
+        super(module);
+
+        this.label = 'topicslist';
+        this.aliases = [ 'topiclist' ];
+
+        this.info = {
+            name: 'topicslist',
+            description: 'Shows all of the available topics.',
+            usage: 'topicslist',
+        };
+
+        this.permissions = new CommandPermissions(this, {
+            staff: {
+                needed: [...this.axon.staff.dailis, ...this.axon.staff.sentries, ...this.axon.staff.admins],
+                bypass: this.axon.staff.owners,
+            },
+        } );
+    }
+    /**
+     * @param {import('axoncore').CommandEnvironment} env
+     */
+
+    async execute({ msg }) {
+      server.findById(msg.guildID, async (err, doc) => {
+        if (err) {
+            return this.sendError(msg.channel, `An error occurred: ${err}`);
+        }
+
+        const topics = doc.data.topics.map((joinMsg, index) => `**${index + 1}:** ${joinMsg}`);
+        const numberOfPages = Math.ceil(topics.length / 20);
+
+        let embeds = [];
+
+        let startingIndex = 0;
+        for (let i=0; i < numberOfPages; i++) {
+            let topicsOnPage = topics.slice(startingIndex, startingIndex + 20);
+            startingIndex += 20; // starting point for next iteration
+
+            embeds.push({ 
+                color: this.utils.getColor('blue'), 
+                title: 'Current Topics',
+                description: topicsOnPage.join('\n'),
+                timestamp: new Date().toISOString()
+            });
+        }
+
+        embedPaginator.createPaginationEmbed(msg, embeds, { timeout: 180000, cycling: true }); // 3 minute timeout
+      });
+    }
+}
+
+module.exports = TopicsList;
+
