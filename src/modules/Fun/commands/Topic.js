86b4a522f0e874ea1daaa13b4d0d460cf86f983a
Nick <nmj3@hotmail.com>
2026-02-13 06:25:38 +1300

    Update Topic.js

--- src/modules/Fun/commands/Topic.js
+++ src/modules/Fun/commands/Topic.js
@@ -13,7 +13,7 @@ const COMMAND_COOLDOWN = 600000;
 const MODERATOR_ROLE_ID = '736365465353453663';
 const NoTopics = [
   //  '805583058631000085' // Moon4
-     '1399822420239716554' // Kat
+   //  '1399822420239716554' // Kat
 ]; // Ignore these users for shits and gigs
 
 const ignoredCategories = [
